8bf12cab55367c04b3b6a9cfef465d76026db81f
Samuel Howell <60439628+SlideHowell@users.noreply.github.com>
2022-10-12 17:49:31 -0400

    adding toastr

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -19,6 +19,10 @@ namespace BulkyBookWeb.Controllers
             return View(objCategoryList);
         }
 
+
+        // NOTE: when modifying controller, must rebuild application. When modifying views, you can just hot reload.
+
+
         // GET action method
         public IActionResult Create()
         {
@@ -30,9 +34,98 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken] // inside any forms it will automaticcally inject key that will be validated 
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj); // add new category obj from form to to the Categories table in the database
-            _db.SaveChanges(); // save the database to "lock in" the new category obj
-            return RedirectToAction("Index"); // sends us back to index view (the view with the category list)
+            // server side validation
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name."); // the first param is name, bc we are adding the error to the name property specifically.
+            }
+            if (ModelState.IsValid) // makes sure data matches the required fields in the Category Model (ex. name must not be null). that way we don't crash the db inserting faulty data.
+            {
+                _db.Categories.Add(obj); // add new category obj from form to to the Categories table in the database
+                _db.SaveChanges(); // save the database to "lock in" the new category obj
+                TempData["success"] = "Category created successfully"; // assign key name succes to Tempdata
+                return RedirectToAction("Index"); // sends us back to index view (the view with the category list)
+            }
+            return View(obj);
+        }
+
+        // GET action method
+        public IActionResult Edit(int? id) // retreive id, and get other details of category based on that id
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id); // finds based on primary key, which we know is id
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id); // will not throw exception if more than one elem found. have top pass generic Category object u
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id); // will throw exception if there are more than one elem found
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST action method
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public IActionResult Edit(Category obj)
+        {
+            // server side validation
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name."); // the first param is name, bc we are adding the error to the name property specifically.
+            }
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Update(obj); 
+                _db.SaveChanges(); 
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index"); 
+            }
+            return View(obj);
+        }
+
+
+        // GET action method
+        public IActionResult Delete(int? id) // retreive id, and get other details of category based on that id
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id); // finds based on primary key, which we know is id
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id); // will not throw exception if more than one elem found. have top pass generic Category object u
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id); // will throw exception if there are more than one elem found
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                
+            _db.Categories.Remove(obj); 
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index"); 
+            
+           
         }
     }
 }
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -9,6 +10,8 @@ namespace BulkyBookWeb.Models
 
         [Required] // makes Name a required property
         public string Name { get; set; }
+        [DisplayName("Display Order")] // this data annotation allows us to use this name in our application, while referecing back to DisplayOrder for db purposes
+        [Range(1,100, ErrorMessage = "You can on;y use numbers between 1 and 100")] // this data annotation forces our range to be between 1 and 100
         public int DisplayOrder { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.Now; // adds a default DateTime of DateTime.Now
 
